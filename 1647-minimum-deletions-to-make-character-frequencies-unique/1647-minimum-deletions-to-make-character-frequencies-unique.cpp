c41ad2dd272be1115363b394bae4c23b2148a309
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-06-28 11:39:26 +0530

    Time: 150 ms (25.68%), Space: 17.5 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        unordered_map<char,int>mp;
+        unordered_map<char,int>mp;       //frequency of each character
         for(char ch:s){
             mp[ch]++;
         }
-        unordered_map<int,int>mp2;
+        unordered_map<int,int>mp2;     // frequency of each character's freq(how many times)
         for(auto it:mp){
             mp2[it.second]++;
         }
-        vector<int>v;
-        for(auto it:mp2){
-            v.push_back(it.first);
-        }
-        sort(v.begin(),v.end());
         int cnt=0;
-        for(int i=0;i<v.size();i++){
-            while(mp2[v[i]]>1){
-                int x=v[i];
+        for(auto it:mp2){
+            while(mp2[it.first]>1){
+                int x=it.first;
                 mp2[x]--;
                 while(mp2.find(x)!=mp2.end()){
                     cnt++;
@@ -28,7 +23,5 @@ public:
             }
         }
         return cnt;
-        
-       
     }
 };
