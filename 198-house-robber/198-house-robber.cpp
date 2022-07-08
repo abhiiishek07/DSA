1339cdc33f5649137e3301c12c73f0a918c1bc02
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-08 20:41:44 +0530

    Time: 3 ms (53.96%), Space: 7.6 MB (75.71%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,8 +1,44 @@
 class Solution {
 public:
-    int solve(int idx,vector<int>nums,vector<int>&dp){
-        if(idx==0)
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)
             return nums[0];
+        int dp[n];
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        
+        for(int i=2;i<n;i++){
+            int pick= nums[i]+dp[i-2];
+            int not_pick= 0+dp[i-1];
+            dp[i]=max(pick,not_pick);
+        }
+        return dp[n-1];
+    }
+};
+
+/* Recursion (will give TLE). TC- 2^n, sc: O(n)
+   
+   int solve(int idx,vector<int>nums){
+        if(idx==0)                //base case
+            return nums[0];
+        if(idx<0)
+            return 0;
+        int pick= nums[idx]+solve(idx-2,nums);
+        int not_pick= 0+solve(idx-1,nums);
+        
+        return max(pick,not_pick);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        return solve(n-1,nums);
+    }
+           ****************
+    MEMOIZATION (accepted). TC: O(n), sc: O(n)+O(n)
+    
+     int solve(int idx,vector<int>nums,vector<int>&dp){
+        if(idx==0)
+            return nums[0];        //base case
         if(idx<0)
             return 0;
         
@@ -19,22 +55,5 @@ public:
         vector<int>dp(n,-1);
         return solve(n-1,nums,dp);
     }
-};
-
-/* Recursion (will give TLE). TC- 2^n, sc: O(n)
-   
-   int solve(int idx,vector<int>nums){
-        if(idx==0)
-            return nums[0];
-        if(idx<0)
-            return 0;
-        int pick= nums[idx]+solve(idx-2,nums);
-        int not_pick= 0+solve(idx-1,nums);
-        
-        return max(pick,not_pick);
-    }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        return solve(n-1,nums);
-    }
+          ****************
     */
