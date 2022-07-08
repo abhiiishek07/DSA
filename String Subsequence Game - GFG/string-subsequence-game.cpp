a9ef43e24fe95589e42dc824822a2ba0aa4995bc
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-08 13:02:00 +0530

    Added solution - LeetHub

---
+++ String Subsequence Game - GFG/string-subsequence-game.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    bool isVowel(char ch){
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    }
+    void solve(string s,int i,string &str,set<string>&ans){
+        if(i>=s.size()){
+            if(str.size()>=2){
+                if(isVowel(str[0]) && !isVowel(str[str.size()-1]))
+                  ans.insert(str);
+            }
+            return;
+        }
+        str.push_back(s[i]);
+        solve(s,i+1,str,ans);
+        str.pop_back();
+        solve(s,i+1,str,ans);
+        
+    }
+    set<string> allPossibleSubsequences(string S) {
+        // code here
+        set<string>ans;
+        string str="";
+        solve(S,0,str,ans);
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        set<string> ans = ob.allPossibleSubsequences(S);
+        if (ans.size() == 0)
+            cout << -1 << endl;
+        else {
+            for (auto i : ans) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+  // } Driver Code Ends
