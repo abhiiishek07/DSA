f3eb8725735889423655d4aef6ab60b2f85c4d82
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-11 17:08:21 +0530

    Time: 23 ms (5.82%), Space: 19 MB (7.88%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>>dp(201,vector<int>(201,-1));
+        int m=triangle.size();
+        for(int j=0;j<triangle[m-1].size();j++){
+            dp[m-1][j]= triangle[m-1][j];
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                dp[i][j]=triangle[i][j]+ min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+/*
+   MEMO (accepted)
+    
+      int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
         if(i==triangle.size()-1)
             return triangle[i][j];
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-            int sum= triangle[i][j]+ min(solve(i+1,j,triangle,dp),solve(i+1,j+1,triangle,dp));
-            return dp[i][j]=sum;
+        int sum= triangle[i][j]+ min(solve(i+1,j,triangle,dp),solve(i+1,j+1,triangle,dp));
+        return dp[i][j]=sum;
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         vector<vector<int>>dp(201,vector<int>(201,-1));
         return solve(0,0,triangle,dp);
     }
-};
+    */
