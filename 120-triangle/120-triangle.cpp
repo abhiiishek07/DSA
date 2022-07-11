12b4f7469f74da446d5cb33cf37ba10060d26aae
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-11 16:30:46 +0530

    Time: 25 ms (5.82%), Space: 18.9 MB (7.88%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>&dp){
+        if(i==triangle.size()-1)
+            return triangle[i][j];
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+            int res=INT_MAX;
+            int sum= triangle[i][j]+ min(solve(i+1,j,triangle,dp),solve(i+1,j+1,triangle,dp));
+            res=min(res,sum);
+            return dp[i][j]=res;
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>>dp(201,vector<int>(201,-1));
+        return solve(0,0,triangle,dp);
+    }
+};
