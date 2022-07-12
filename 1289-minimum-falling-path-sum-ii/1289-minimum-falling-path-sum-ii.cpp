45e162136f4d58b676e2f0142ab00a6f0004cb95
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-12 15:02:50 +0530

    Time: 2011 ms (5.07%), Space: 16 MB (6.83%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,83 +1,31 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<int>last(n,0);
-        for(int i=0;i<n;i++)
-            last[i]=grid[0][i];
-        
-        for(int i=1;i<n;i++){
-            vector<int>curr(n,0);
-            for(int j=0;j<n;j++){
-                int prev=j,mini=INT_MAX;
-                for(int k=0;k<n;k++)
-                   if(k!=prev){
-                    int sum= grid[i][prev]+last[k];
-                       mini=min(mini,sum);
-                }
-                 curr[j]=mini; 
-            }
-            last=curr;
-        }
-         int res=INT_MAX;
-        for(int lastRow=0;lastRow<n;lastRow++){
-            int potAns= last[lastRow];
-            res=min(res,potAns);
-        }
-        return res;
-        
-    }
-};
-
-/*  TABULATION (accepted :) )
-
-       int minFallingPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++)
-            dp[0][i]=grid[0][i];
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n;j++){
-                int prev=j,mini=INT_MAX;
-                for(int k=0;k<n;k++)
-                   if(k!=prev){
-                    int sum= grid[i][prev]+dp[i-1][k];
-                    mini=min(mini,sum);
-                }
-                dp[i][prev]=mini;
-            }
-        }
-         int res=INT_MAX;
-        for(int lastRow=0;lastRow<n;lastRow++){
-            int potAns= dp[n-1][lastRow];
-            res=min(res,potAns);
-        }
-        return res;
-    }
-    
-           *********************
-
-MEMO (accepted but very high run time)
-
-        int solve(int i,int prev,vector<vector<int>>&grid,vector<vector<int>>&dp){
+    int solve(int i,int prev,vector<vector<int>>& grid,vector<vector<int>>&dp){
         if(i==0){
-            return grid[i][prev];
+            int mini=INT_MAX;
+            for(int c=0;c<grid[0].size();c++){
+                if(c!=prev)
+                    mini=min(mini,grid[0][c]);
+            }
+            return mini; 
         }
         if(dp[i][prev]!=-1)
             return dp[i][prev];
         int mini=INT_MAX;
-        for(int c=0;c<grid.size();c++){
+        for(int c=0;c<grid[i].size();c++){
             if(c!=prev){
-                int sum= grid[i][prev]+ solve(i-1,c,grid,dp);
+                int sum= grid[i][c]+ solve(i-1,c,grid,dp);
                 mini=min(mini,sum);
             }
         }
         return dp[i][prev]=mini;
+        
     }
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        if(n==1)
+            return grid[0][0];
+        vector<vector<int>>dp(n,vector<int>(201,-1));
         int res=INT_MAX;
         for(int lastRow=0;lastRow<n;lastRow++){
             int potAns= solve(n-1,lastRow,grid,dp);
@@ -85,4 +33,4 @@ MEMO (accepted but very high run time)
         }
         return res;
     }
-*/
+};
