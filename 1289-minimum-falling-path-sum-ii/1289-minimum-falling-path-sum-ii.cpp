38a5c8d1e88899fd0027fddfacde2702f4273448
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-12 14:29:27 +0530

    Time: 1468 ms (12.02%), Space: 15.9 MB (36.35%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int solve(int i,int prev,vector<vector<int>>& grid,vector<vector<int>>&dp){
+    int solve(int i,int prev,vector<vector<int>>&grid,vector<vector<int>>&dp){
         if(i==0){
-            int mini=INT_MAX;
-            for(int c=0;c<grid[0].size();c++){
-                if(c!=prev)
-                    mini=min(mini,grid[0][c]);
-            }
-            return mini; 
+            return grid[i][prev];
         }
         if(dp[i][prev]!=-1)
             return dp[i][prev];
         int mini=INT_MAX;
-        for(int c=0;c<grid[i].size();c++){
+        for(int c=0;c<grid.size();c++){
             if(c!=prev){
-                int sum= grid[i][c]+ solve(i-1,c,grid,dp);
+                int sum= grid[i][prev]+ solve(i-1,c,grid,dp);
                 mini=min(mini,sum);
             }
         }
         return dp[i][prev]=mini;
-        
     }
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
-        if(n==1)
-            return grid[0][0];
-        vector<vector<int>>dp(n,vector<int>(201,-1));
+        vector<vector<int>>dp(n,vector<int>(n,-1));
         int res=INT_MAX;
         for(int lastRow=0;lastRow<n;lastRow++){
             int potAns= solve(n-1,lastRow,grid,dp);
