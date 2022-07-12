2fda92a0bb59cc06254cddfc68aa9c785001057c
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-12 10:58:23 +0530

    Time: 20 ms (62.71%), Space: 10.4 MB (24.64%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>&dp){
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++)
+            dp[0][i]=matrix[0][i];
+        
+            for(int i=1;i<n;i++){
+                for(int j=0;j<n;j++){
+                    int dl,u,dr;
+                   dl= j-1>=0? dp[i-1][j-1]:INT_MAX;
+                    u=dp[i-1][j];
+                   dr= j+1<n ? dp[i-1][j+1]:INT_MAX;
+                    dp[i][j]=matrix[i][j]+ min({dl,u,dr});
+                }
+            }
+       int res=INT_MAX;
+        for(int j=0;j<n;j++)
+            res=min(res,dp[n-1][j]);
+        return res;
+    }
+};
+
+/* MEMO (accepted)
+
+  int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>&dp){
          if(j<0||j>=matrix.size())
             return INT_MAX; 
         if(i==0){
@@ -8,9 +32,11 @@ public:
         }
         if(dp[i][j]!=-1)
             return dp[i][j];
-        int digLeft=solve(i-1,j-1,matrix,dp);
-        int up=solve(i-1,j,matrix,dp);
-        int digRight=solve(i-1,j+1,matrix,dp);
+            
+        int digLeft=solve(i-1,j-1,matrix,dp);    // diagonal left
+        int up=solve(i-1,j,matrix,dp);           // up
+        int digRight=solve(i-1,j+1,matrix,dp);   // diagonal Right
+        
         dp[i][j]=matrix[i][j]+min({digLeft,up,digRight});
         return dp[i][j];     
     }
@@ -22,4 +48,4 @@ public:
             res=min(res,solve(n-1,j,matrix,dp));
         return res;
     }
-};
+*/
