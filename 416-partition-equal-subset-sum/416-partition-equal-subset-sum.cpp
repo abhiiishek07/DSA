aaa37702247d5179f6569e000498e987ff949bdf
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-15 12:44:48 +0530

    Time: 425 ms (31.51%), Space: 11.9 MB (60.75%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
-    bool solve(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum= accumulate(nums.begin(),nums.end(),0);
+        if(sum%2!=0)
+            return false;
+        vector<vector<bool>>dp(n,vector<bool>((sum/2)+1,false));
+        for(int i=0;i<n;i++)
+            dp[i][0]=true;
+        if(nums[0]<sum/2)
+          dp[0][nums[0]]=true;
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum/2;target++){
+                bool not_pick= dp[i-1][target];
+                bool pick=false;
+                if(nums[i]<=target)
+                   pick= dp[i-1][target-nums[i]];
+            dp[i][target]= pick | not_pick;
+            }
+        }
+        return dp[n-1][sum/2];
+    }
+};
+/* MEMO (accepted)
+
+  bool solve(int idx,int target,vector<int>&nums,vector<vector<int>>&dp){
         if(target==0)
             return true;
         if(idx==0){
@@ -22,4 +46,4 @@ public:
         vector<vector<int>>dp(n,vector<int>((sum/2)+1,-1));
         return solve(n-1,sum/2,nums,dp);
     }
-};
+    */
