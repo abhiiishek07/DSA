fff9ccbbbffca660d91e74c8c4fe62bc84c90a8e
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-20 17:31:04 +0530

    Time: 1051 ms (39.48%), Space: 169.6 MB (14.70%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    void solve(int firstEle,unordered_map<int,vector<int>>&adj,unordered_map<int,int>&visited,vector<int>&ans){
+    void solve(int firstEle,unordered_map<int,vector<int>>&adj,unordered_set<int>&visited,vector<int>&ans){
         ans.push_back(firstEle);
-        visited[firstEle]++;
+        visited.insert(firstEle);
         for(int x:adj[firstEle]){
             if(visited.find(x)==visited.end())
                 solve(x,adj,visited,ans);
@@ -10,27 +10,21 @@ public:
         return;
     }
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-       unordered_map<int,int>mp;
-        for(auto it:adjacentPairs){
-            mp[it[0]]++;
-            mp[it[1]]++;
-        }
-        int firstEle;
-        for(auto it:mp){
-            if(it.second==1){
-                firstEle=it.first;
-                break;
-          }
-        }
         unordered_map<int,vector<int>>adj;
         for(auto it:adjacentPairs){
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        unordered_map<int,int>visited;
+        int firstEle;
+        for(auto it:adj){
+            if(it.second.size()==1){
+                firstEle=it.first;
+                break;
+            }
+        }
+        unordered_set<int>visited;
         vector<int>ans;
         solve(firstEle,adj,visited,ans);
         return ans;
-        
     }
 };
