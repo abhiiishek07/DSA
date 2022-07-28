601278c4b17fc303bb16a7d1c2d642d5b4fa5e97
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-28 21:14:16 +0530

    Time: 11 ms (80.62%), Space: 10.6 MB (33.43%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    void solve(int s,vector<vector<int>>adj,vector<int>&vis){
+    void solve(int s,vector<vector<int>>& rooms,vector<int>&vis){
         vis[s]=1;
-        for(int v:adj[s]){
+        for(int v:rooms[s]){
             if(!vis[v])
-                solve(v,adj,vis);
+                solve(v,rooms,vis);
         }
         return;
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
         vector<int>vis(n,0);
-        vector<vector<int>>adj(n);
-       for(int i=0;i<n;i++){
-           for(int j=0;j<rooms[i].size();j++){
-               adj[i].push_back(rooms[i][j]);
-           }
-       }
-        solve(0,adj,vis);
+        solve(0,rooms,vis);
         for(int x:vis){
             if(!x)
                 return false;
