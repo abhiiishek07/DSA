983bb2677ced5151788eb1880fe760718dacb6ed
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-07-31 11:18:24 +0530

    Time: 30 ms (100.00%), Space: 9.7 MB (100.00%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int x:nums){
+            if(x>0)
+                pq.push(x);
+        }
+        int cnt=0;
+        while(!pq.empty()){
+            int s=pq.top();
+            pq.pop();
+            cnt++;
+            priority_queue<int,vector<int>,greater<int>>pq2;
+            while(!pq.empty()){
+                int x=pq.top();pq.pop();
+                if(x-s >0)
+                    pq2.push(x-s);
+            }
+           while(!pq2.empty()){
+               pq.push(pq2.top());
+               pq2.pop();
+           }
+        }
+        return cnt;
+    }
+};
