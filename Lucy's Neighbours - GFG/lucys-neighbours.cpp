e7fd89464e9a6ee8a2beb2a7f3193b2f7c78c66a
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2022-10-29 16:14:12 +0530

    Added solution - LeetHub

---
+++ Lucy's Neighbours - GFG/lucys-neighbours.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    vector<int> Kclosest(vector<int>arr, int n, int x, int k) 
+    { 
+        // Your code goes here 
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(int i=0;i<n;i++){
+            pq.push({abs(x-arr[i]),arr[i]});
+        }
+        vector<int>ans;
+        while(!pq.empty() && k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,x,k;
+		cin>>n>>x>>k; 
+		vector <int> array(n);
+		for (int i = 0; i < n; ++i)
+			cin>>array[i];
+        
+        Solution obj;
+		vector <int> result = obj.Kclosest(array, n, x, k); 
+		for (int i = 0; i < result.size(); ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+
+	return 0; 
+} 
+
+
+
+
+// } Driver Code Ends
