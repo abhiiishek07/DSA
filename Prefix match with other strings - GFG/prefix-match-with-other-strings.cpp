0fbff62d5d8bee91f35ed6bc3a5dc02110402203
Abhishek Kumar <84977074+abhiiishek07@users.noreply.github.com>
2023-01-05 21:29:32 +0530

    Added solution - LeetHub

--- Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
@@ -6,9 +6,68 @@ using namespace std;
 
 // } Driver Code Ends
 //User function Template for C++
-
+struct Node{
+    int cnt;
+    vector<Node*>children;
+    bool isEnd;
+    Node(){
+        children= vector<Node*>(26,NULL);
+        cnt=0;
+        isEnd=false;
+    }
+    bool containsKey(char ch){
+        return children[ch-'a']!=NULL;
+    }
+    void put(char ch, Node* newNode){
+        children[ch-'a']= newNode;
+    }
+    Node* getNext(char ch){
+        return children[ch-'a'];
+    }
+};
+class Trie{
+    public: Node* root;
+    Trie(){
+        root= new Node();
+    }
+    void insert(string word){
+        Node* node= root;
+        for(char ch:word){
+            if(!node->containsKey(ch)){
+                node->put(ch, new Node());
+            }
+            
+            node= node->getNext(ch);
+            node->cnt++;
+        }
+        node->isEnd=true;
+    }
+    int allPrefix(string str){
+        Node* node=root;
+        for(char ch:str){
+             node= node->getNext(ch);
+            if(node->isEnd) break;
+           
+        }
+        
+        return node->cnt;
+    }
+};
 class Solution{   
 public:
+    int klengthpref(string arr[], int n, int k, string str){    
+        Trie trie;
+        for(int i=0;i<n;i++){
+            trie.insert(arr[i]);
+        }
+        if(k>str.size()) return 0;
+        str= str.substr(0,k);
+        int ans= trie.allPrefix(str);
+        return ans;
+        
+    }
+    /* Accepted answer
+    
     int klengthpref(string arr[], int n, int k, string str){    
       if(k>str.size()) return 0;
       str= str.substr(0,k);
@@ -20,6 +79,8 @@ public:
       }
       return ans;
     }
+    */
+    
 };
 
 //{ Driver Code Starts.
